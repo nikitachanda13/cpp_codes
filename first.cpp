35e6c13613bfc6a7b62ab84befd56ecb23da6b6d
NikitaChanda <nikitachanda@NikitaChandas-MacBook-Air.local>
2026-01-26 20:03:23 +0530

    Initial Commit: C++ practise codes

---
+++ first.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout<<"this is my first program"<< "\n" ;
+    cout<<"Nikita"<<"\n"<<"Chanda"<<"\n";
+    int age;
+    cout<<"enter your age"<<"\n";
+    cin >> age;
+    cout<<"your age is: "<<age<<"\n";
+    return 0;
+}
---
+++ sum.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cout << "Enter a and b: ";
+    cin >> a >> b;
+    cout << "Sum = " << a + b << endl;
+    cout << "Program ended successfully." << endl;
+    return 0;
+}
