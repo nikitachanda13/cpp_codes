8da770815303622a59ac074e66cbe1d6fbdd1c10
NikitaChanda <nikitachanda@NikitaChandas-MacBook-Air.local>
2026-01-27 10:24:54 +0530

    Day 2

---
+++ vote.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int age;
+    cout<<"enter your age:  ";
+    cin>>age;
+    if(age>=18){
+        cout<<"you can vote"<<"\n";
+
+    }
+    else{
+        cout<<"you cannot vote"<<"\n";
+    }
+    return 0;
+}
