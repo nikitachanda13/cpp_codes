e10aab37b26fa6affb8a6b976568fcb6b82ce99c
Nikita <nikitachanda13@gmail.com>
2026-03-11 20:15:42 +0530

    Patterns

---
+++ pattern2.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter the number of n: "<<endl;
+    cin>>n;
+    for(int i=0; i<=n-1; i++){
+        char ch= 'A';
+        for(int j=0; j<n-1; j++){
+            cout<<ch;
+            ch = ch+1;
+        }
+        cout<<endl;
+    }
+    return 0;
+}
